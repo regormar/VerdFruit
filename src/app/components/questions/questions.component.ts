1966238eeb03d6b77f2a1a79df5652fc0af865eb
JosepMariaProfitos <josepprofitos99@gmail.com>
2021-04-05 18:01:36 +0200

    Pestaña de preguntas frecuentes entera, con sus correspondientes traducciones.

--- src/app/components/questions/questions.component.ts
+++ src/app/components/questions/questions.component.ts
@@ -1,5 +1,6 @@
 
 import { Component, OnInit } from "@angular/core";
+import { TranslateService } from "@ngx-translate/core";
 import { CookiesServiceController } from "src/app/services/cookies.service";
 import { UserServiceController } from "src/app/services/user.service";
 
@@ -13,6 +14,23 @@ import { UserServiceController } from "src/app/services/user.service";
 
 
 export class QuestionsComponent {
-    
+    constructor(private _service:UserServiceController, private translate: TranslateService){}
 
+    ngOnInit(): void {
+        var acc = document.getElementsByClassName("pregunta");
+        var i;
+
+        for (i = 0; i < acc.length; i++) {
+            acc[i].addEventListener("click", function() {
+                this.classList.toggle("active");
+                var panel = this.nextElementSibling;
+                if (panel.style.maxHeight) {
+                panel.style.maxHeight = null;
+                } else {
+                panel.style.maxHeight = panel.scrollHeight + "px";
+                } 
+            });
+        }
+    }
+    
 }
