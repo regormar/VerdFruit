48a83e8129fb2b1d09341c929e49f5b002890f7a
JosepMariaProfitos <josepprofitos99@gmail.com>
2021-04-13 16:11:09 +0200

    Datos personales del perfil de usuraio

--- src/app/components/cuenta/cuenta.component.ts
+++ src/app/components/cuenta/cuenta.component.ts
@@ -1,6 +1,7 @@
 
 import { Component, OnInit } from "@angular/core";
 import { TranslateService } from "@ngx-translate/core";
+import { Usuario } from "src/app/model/usuario";
 import { CookiesServiceController } from "src/app/services/cookies.service";
 import { LogoutServiceController } from "src/app/services/logout.service";
 import { UserServiceController } from "src/app/services/user.service";
@@ -18,22 +19,39 @@ export class CuentaComponent {
     constructor(private _service:UserServiceController, private translate: TranslateService,
         private _logoutService:LogoutServiceController){}
 
-    opcion:string = "datos";
-
+    opcion:string = "DATOS";
+    idUsuario:string = "";
+    usuario:Usuario;
+    
     ngOnInit(): void {
         this.changeDatos(this.opcion);
-    }
+        this.idUsuario = sessionStorage.getItem("_id");
+        console.log(sessionStorage.getItem("_id"));
+    }2
 
     changeDatos(opcion:string){
         this.opcion = opcion;
-        var element = document.getElementById("particular");
-        var element2 = document.getElementById("business");
-        if(opcion === "particular"){            
-            element?.classList.add("selected");
-            element2?.classList.remove("selected");
-        }else{
-            element?.classList.remove("selected");
-            element2?.classList.add("selected");
+        if(opcion === "DATOS"){   
+            
+            console.log(sessionStorage.getItem("_id"));
+            let id = sessionStorage.getItem("_id");
+            console.log(id);
+            this._service.getUsuarioById(sessionStorage.getItem("_id"))
+            .subscribe(
+                (result) => {
+                    console.log(result);
+                    if(result = 200){
+                        this.usuario = result;
+                        console.log(this.usuario);
+                    }else{
+                        console.log("Error: no se");
+                    }
+                    
+                },
+                (error) => { 
+                    console.log(error);
+                }
+            );
         }
     }
 
