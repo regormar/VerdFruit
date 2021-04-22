ebe7017f0d3e5ec2bad9619767193c56af18495a
JosepMariaProfitos <josepprofitos99@gmail.com>
2021-04-22 16:41:43 +0200

    Perfil del usuario.

--- src/app/components/cuenta/cuenta.component.ts
+++ src/app/components/cuenta/cuenta.component.ts
@@ -1,5 +1,6 @@
 
 import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
 import { TranslateService } from "@ngx-translate/core";
 import { Usuario } from "src/app/model/usuario";
 import { CookiesServiceController } from "src/app/services/cookies.service";
@@ -14,20 +15,23 @@ import { UserServiceController } from "src/app/services/user.service";
     providers:[UserServiceController, CookiesServiceController, LogoutServiceController],
 })
 
-
 export class CuentaComponent {
     constructor(private _service:UserServiceController, private translate: TranslateService,
-        private _logoutService:LogoutServiceController){}
+        private _router: Router, private _logoutService:LogoutServiceController){
+            if(sessionStorage.getItem("_id") == null){
+                this._router.navigate(['/login']);
+            }
+        }
 
     opcion:string = "DATOS";
     idUsuario:string = "";
-    usuario:Usuario;
+    usuario:Usuario = null;
     
     ngOnInit(): void {
         this.changeDatos(this.opcion);
         this.idUsuario = sessionStorage.getItem("_id");
         console.log(sessionStorage.getItem("_id"));
-    }2
+    }
 
     changeDatos(opcion:string){
         this.opcion = opcion;
@@ -39,12 +43,12 @@ export class CuentaComponent {
             this._service.getUsuarioById(sessionStorage.getItem("_id"))
             .subscribe(
                 (result) => {
-                    console.log(result);
-                    if(result = 200){
+                    if(result == null){
+                        console.log("Error: no se");
+                    }else{
+                        console.log(result);
                         this.usuario = result;
                         console.log(this.usuario);
-                    }else{
-                        console.log("Error: no se");
                     }
                     
                 },
